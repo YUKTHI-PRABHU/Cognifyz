9c5ae8ff8c6652680f315b25ba468e5711146252
YUKTHI PRABHU <yukthiprabhu160421@gmail.com>
2024-01-31 23:18:46 +0530

    first

---
+++ Task 1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Welcome to the Guessing Game!")
+print("I'm thinking of a number between 1 and 10.")
+print("Can you guess what it is?")
+print("You will be given 5 chances to check your luck\n")
+
+import random
+
+def start_game():
+    secret_number = random.randint(1, 10)
+    attempts = 0
+
+   
+    while True:
+       
+        if attempts>=5:
+            print(" You are done with all 5 chances !!Try next time All the best") 
+            break
+
+            
+        guess = input("Enter your guess: ")
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+            
+      
+        
+    
+        attempts += 1
+        
+        
+        
+
+        if guess >10:
+            print("out of limit\n")
+        elif guess < secret_number:
+            print("Too low!Please guess high value\n")
+        elif guess > secret_number:
+            print("Too high! Please guess low value\n")
+       
+        else:
+            print("Congratulations! You guessed the number in {} attempts.".format(attempts))
+            print("Random number was:",secret_number)
+            break
+
+start_game()
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Task 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = int(input("Enter the number of rows"))         
+for i in range(0, n):     
+        for j in range(0, i + 1):      
+            print("* ", end="")        
+        print()
+
+
+# In[ ]:
+
+
+
+
