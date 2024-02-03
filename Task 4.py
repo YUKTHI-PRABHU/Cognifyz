9af4be1e968a190e34c220c6256d34202b00b30d
Yukthi Prabhu <135090816+YUKTHI-PRABHU@users.noreply.github.com>
2024-02-03 09:48:44 +0530

    Add files via upload

---
+++ Task 4.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+temperature = int(input("Enter temperature value "))
+select = input("Enter the notation for the entered temperature (celsius) C or (Fahrenheit) F (Kelvin) K ? " )
+if select.upper() == "F":
+    x = int(round((temperature-32)/9 *5))
+    print("Temerature",temperature,select)
+    print(x,"C")
+if select.upper() == "F":
+    x = int(round(((temperature-32)* 5/9)+ 273.15)  )   
+    print(x,"K")
+if select.upper()=='C':
+    x = int(round((temperature *9) /5 +32))
+    print(x, "F")
+if select.upper()=='C':
+    x = int(round((temperature +273.15)))
+    print(x, "K")
+if select.upper()=='K':
+    x = int(round((temperature -273.15)))
+    print(x, "C")
+if select.upper()=='K':
+    x = int(round((temperature -273.15)*9/5 + 32))                    
+    print(x, "F")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
